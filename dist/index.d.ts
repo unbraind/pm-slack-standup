f4099e885bd2d77b1b82a70d327f89c10cb44dd8
Stefan Preu <stefan@preu.at>
2026-05-09 13:57:13 +0200

    build: compile TypeScript to dist/index.js for pm extension install

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+    name: string;
+    version: string;
+    activate(api: import("@unbrained/pm-cli/sdk").ExtensionApi): void;
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
